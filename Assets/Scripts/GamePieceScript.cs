1bdf35db2785bd7b375db8ed3776dff71058b287
draino7n7 <kevin_audrain@yahoo.com>
2024-04-05 20:09:02 -0400

    Created a game piece prefab, script and material.

--- Assets/Scripts/GamePieceScript.cs
+++ Assets/Scripts/GamePieceScript.cs
@@ -2,15 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpaceScript : MonoBehaviour
+public class GamePieceScript : MonoBehaviour
 {
-    public int row;
-    public int col;
-
     // Start is called before the first frame update
     void Start()
     {
-        this.GetComponent<MeshRenderer>().enabled = false;
+        
     }
 
     // Update is called once per frame
