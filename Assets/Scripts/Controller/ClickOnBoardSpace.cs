c7d2680114fa6f6906d900a3962617adfd4221e8
draino7n7 <kevin_audrain@yahoo.com>
2025-02-02 10:52:08 -0500

    Handle clicking on board space.

--- Assets/Scripts/Controller/ClickOnBoardSpace.cs
+++ Assets/Scripts/Controller/ClickOnBoardSpace.cs
@@ -12,10 +12,32 @@ namespace Controller
 
         }
 
-        // Update is called once per frame
         void Update()
         {
+            // Detect left mouse button click
+            if (Input.GetMouseButtonDown(0))
+            {
+                // Raycast from the mouse position
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
 
+                if (Physics.Raycast(ray, out hit))
+                {
+                    // Check if the object hit has the SpaceView script
+                    View.BoardSpace spaceView = hit.collider.gameObject.GetComponent<View.BoardSpace>();
+                    if (spaceView != null)
+                    {
+                        Debug.Log("SpaceView object clicked: " + hit.collider.gameObject.name);
+                        HandleClick(spaceView);
+                    }
+                }
+            }
+        }
+
+        private void HandleClick(View.BoardSpace spaceView)
+        {
+            // Add logic to handle what happens when the object is clicked
+            Debug.Log($"Handling click on: {spaceView.gameObject.name} at row {spaceView.Row} and column {spaceView.Col}");
         }
     }
 }
--- Assets/Scripts/View/BoardSpace.cs
+++ Assets/Scripts/View/BoardSpace.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 namespace View
 {
-    public class SpaceView : MonoBehaviour
+    public class BoardSpace : MonoBehaviour
     {
         [SerializeField]    private int row;
-        [SerializeField]    public int col;
+        [SerializeField]    private int col;
+
+        public int Row { get { return row; } }
+        public int Col { get { return col; } } 
 
         // Start is called before the first frame update
         void Start()
